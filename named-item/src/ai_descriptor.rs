69d665fa0ac2c91a8a65e93e174ba8793a9e2a74
klebs6 <tpk3.mx@gmail.com>
2024-11-21 19:04:40 -0500

    update named-item dep

--- named-item/src/ai_descriptor.rs
+++ named-item/src/ai_descriptor.rs
@@ -6,7 +6,7 @@ pub trait ItemFeature {
 
 pub trait ItemWithFeatures {
     fn header(&self) -> Cow<'_,str>;
-    fn features(&self) -> &[Cow<'_,str>];
+    fn features(&self) -> Vec<Cow<'_, str>>;
 }
 
 pub trait AIDescriptor {
@@ -41,7 +41,7 @@ mod tests {
             Cow::Borrowed(&self.header)
         }
 
-        fn features(&self) -> &[Cow<'_, str>] {
+        fn features(&self) -> Vec<Cow<'_, str>> {
             &self.features
         }
     }
