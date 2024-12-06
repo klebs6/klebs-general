1bd97c9c0b997c75cf12d7db099d2395def36a65
klebs6 <tpk3.mx@gmail.com>
2024-12-05 21:13:28 -0500

    add OpenConversation trait

--- ai-descriptor/src/lib.rs
+++ ai-descriptor/src/lib.rs
@@ -2,3 +2,7 @@ pub use ai_descriptor_derive::*;
 pub use ai_descriptor_trait::*;
 pub use plural_derive::*;
 pub use plural_trait::*;
+
+pub trait OpenConversation {
+    fn open_conversation(&self) -> &'static str;
+}
