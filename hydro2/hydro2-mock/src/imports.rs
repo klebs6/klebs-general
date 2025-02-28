4db1d7382a74f94b5225d28a28a970179e7bfd43
klebs6 <tpk3.mx@gmail.com>
2025-02-28 01:17:16 -0500

    remove hydro2-async-scheduler dep from hydro2-mock

--- hydro2/hydro2-mock/src/imports.rs
+++ hydro2/hydro2-mock/src/imports.rs
@@ -1,6 +1,5 @@
 // ---------------- [ File: src/imports.rs ]
 pub(crate) use hydro2_3p::*;
-pub(crate) use hydro2_async_scheduler::*;
 pub(crate) use hydro2_basic_operators::*;
 pub(crate) use hydro2_network::*;
 pub(crate) use hydro2_operator::*;
