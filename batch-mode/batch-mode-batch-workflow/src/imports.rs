b5d5a45c00b6bd785b7464bc8cc4fcb63e765f4c
klebs6 <tpk3.mx@gmail.com>
2025-03-31 14:43:37 -0400

    fix missing dep batch-mode-batch-workflow

--- batch-mode/batch-mode-batch-workflow/src/imports.rs
+++ batch-mode/batch-mode-batch-workflow/src/imports.rs
@@ -1,5 +1,7 @@
+
 // ---------------- [ File: batch-mode-batch-workflow/src/imports.rs ]
 pub(crate) use batch_mode_3p::*;
+pub(crate) use batch_mode_batch_client::*;
 pub(crate) use batch_mode_batch_reconciliation::*;
 pub(crate) use batch_mode_batch_scribe::*;
 pub(crate) use batch_mode_batch_workspace_interface::*;
